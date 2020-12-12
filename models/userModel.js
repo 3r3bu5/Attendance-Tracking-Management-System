73f1a4f407758bf95c0206509141c1c8e898d72a
Ashraf <deva4rf@gmail.com>
2020-12-12 04:44:47 -0800

    Added check-in & checkout endpoints

--- models/userModel.js
+++ models/userModel.js
@@ -36,16 +36,44 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    avaliable: {
-      type: Boolean,
-      default: false,
-    },
     imageURL: {
       type: String,
     },
+    absenceDays: {
+      type: Number,
+      default: 0,
+    },
     attendance: [
       {
-        type: String,
+        avaliable: {
+          type: Boolean,
+          default: false,
+        },
+        workingHours: {
+          type: Number,
+          default: 0,
+        },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+        entry: { type: Date },
+        exit: {
+          time: { type: Date },
+          reason: {
+            // 1 for normal exit (end of the day)
+            // 2 for exit with request
+            type: Number,
+            default: 1,
+            min: 1,
+            max: 2,
+          },
+        },
+        hasLVR: {
+          type: Boolean,
+          default: false,
+        },
+        LVRID: String,
       },
     ],
   },
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const { now } = require("mongoose");
 var router = express.Router();
 
 // import usersModel
@@ -122,6 +123,184 @@ router.delete("/:userId", (req, res, next) => {
         res.setHeader("content-type", "application/json");
         res.json({ message: "Deleted successfully" });
       } else {
+        err = new Error("user doesn't exists");
+        err.statusCode = 404;
+        return next(err);
+      }
+    })
+    .catch((err) => next(err));
+});
+
+/*
+@Route      >    METHOD /users/:userId/enter  /leave
+@Behavioure >    check in for a specific user / 
+                 checkout for a specific user / 
+                 
+@Access     >    Specific user to checkin /
+                 Specific user to checkout /
+                 
+*/
+
+router.post("/:userId/enter", (req, res, next) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (user != null) {
+        const data = {
+          entry: Date.now(), // save current time as check-in entry
+          avaliable: true,
+        };
+        startinghour = 09; // startinghour of the day (remember to use 2 digits ex: if the starting hour is 9 use 09 NOT 9)
+        allowanceminutes = 10; // minutes allowed to check in after workinghour
+
+        /*
+            get current hour and minutes to check if the employee can check-in at this time
+          */
+        currentDate = new Date();
+        currentHour = currentDate.toUTCString().substr(17, 2);
+        currentMinutes = currentDate.getMinutes();
+        console.log(currentHour, currentMinutes);
+
+        if (user.attendance && user.attendance.length > 0) {
+          // check if employee has attendance history
+
+          /*
+            grab the last attendance day of the employee
+            to check if he already signed in for today. 
+            or 24 hrs passed from his last entry attendance 
+            so he can check-in for today
+          */
+          const lastCheckIn = user.attendance[user.attendance.length - 1];
+          const lastCheckInTimestamp = lastCheckIn.date.getTime();
+
+          const nextCheckIn = new Date(lastCheckInTimestamp);
+          nextCheckIn.setHours(nextCheckIn.getHours() + 24); // grab the timestamp of the next day of last attendance entry
+
+          if (currentDate.getTime() >= nextCheckIn.getTime()) {
+            // if 24 hrs passed from his last entry
+
+            if (
+              currentHour === startinghour &&
+              currentMinutes <= allowanceminutes
+            ) {
+              user.attendance.push(data);
+              user
+                .save()
+                .then((user) => {
+                  res.status(200);
+                  res.setHeader("content-type", "application/json");
+                  res.json({ message: "DONE! You have signed in today" });
+                })
+                .catch((err) => next(err));
+            } else {
+              res.status(200);
+              res.setHeader("content-type", "application/json");
+              res.json({ message: "Sorry, You can't check in at this time" });
+            }
+          } else {
+            res.status(200);
+            res.setHeader("content-type", "application/json");
+            res.json({ message: "You are already signed in for today" });
+          }
+        } else {
+          console.log(currentHour == startinghour);
+          if (
+            currentHour == startinghour &&
+            currentMinutes <= allowanceminutes
+          ) {
+            user.attendance.push(data);
+            user
+              .save()
+              .then((user) => {
+                res.status(200);
+                res.setHeader("content-type", "application/json");
+                res.json({ message: "DONE! You have signed in today" });
+              })
+              .catch((err) => next(err));
+          } else {
+            res.status(200);
+            res.setHeader("content-type", "application/json");
+            res.json({ message: "Sorry, You can't check in at this time" });
+          }
+        }
+      } else {
+        err = new Error("user doesn't exists");
+        err.statusCode = 404;
+        return next(err);
+      }
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/:userId/leave", (req, res, next) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (user != null) {
+        workHours = 8; // number of work hours of each day
+        /*
+            get current hour and minutes to check if the employee completed workhours of the day
+        */
+        currentDate = new Date();
+        currentHour = currentDate.toUTCString().substr(17, 2);
+        currentMinutes = currentDate.getMinutes();
+        console.log(currentHour, currentMinutes);
+
+        if (user.attendance && user.attendance.length > 0) {
+          // check if employee has attendance history
+
+          /*
+            grab the last attendance day of the employee
+            to check if he already signed out for today. 
+          
+          */
+          const lastAttendance = user.attendance[user.attendance.length - 1];
+          const lastAttendanceEntryTimestamp = lastAttendance.entry.getTime();
+
+          if (lastAttendance.exit.time) {
+            //means that user has signed out for today
+            res.status(200);
+            res.setHeader("content-type", "application/json");
+            res.json({ message: "You are already signed out for today!" });
+          } else {
+            // calculate the employee working hours from entry date to exit date
+            empWorkedHours =
+              (currentDate.getTime() - lastAttendanceEntryTimestamp) / 1000;
+            empWorkedHours /= 60 * 60;
+            console.log(empWorkedHours);
+
+            if (empWorkedHours >= workHours) {
+              // check if employee has completed the specified workhours
+              lastAttendance.exit.time = Date.now();
+              lastAttendance.avaliable = false;
+              lastAttendance.workingHours = Math.round(empWorkedHours);
+              user
+                .save()
+                .then((user) => {
+                  res.status(200);
+                  res.setHeader("content-type", "application/json");
+                  res.json({ message: "DONE! You have signed out for today" });
+                })
+                .catch((err) => next(err));
+            } else {
+              // employee didn't complete the workhours
+              res.status(200);
+              res.setHeader("content-type", "application/json");
+              res.json({
+                message:
+                  "You can't sign out yet!, there is work to do :)) or you can apply for leaving request",
+              });
+            }
+          }
+        } else {
+          // employee has no attendance entry
+          res.status(200);
+          res.setHeader("content-type", "application/json");
+          res.json({
+            message:
+              "Sorry, You cant sign out without being signed in for today!",
+          });
+        }
+      } else {
+        // employee doesn't exist
         err = new Error("user doesn't exists");
         err.statusCode = 404;
         return next(err);
